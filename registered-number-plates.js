410739d3f7eb8711a0dc15648e69492cba68dd79
gisioraelvis <gisioraelvis20@gmail.com>
2023-01-18 08:45:50 +0300

    registered number plates

---
+++ registered-number-plates.js
@@ -0,0 +1,5 @@
+/**
+ * CHALLENGE:
+ * Write an application that can take in two Kenyan license plates (Kxx-000x)
+ * as parameters and find how many plates have been registered between the two plates.
+ * */
