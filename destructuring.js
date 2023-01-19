72c155bf6417d4fa62de0adf7721165a4ab02660
gisioraelvis <gisioraelvis20@gmail.com>
2023-01-19 09:55:02 +0300

    added a demo of destructuring

--- destructuring.js
+++ destructuring.js
@@ -1,35 +1,7 @@
-/* 
-[Yesterday 3:38 PM] Jonathan Ndambuki
- 
-  {
-    "id": 2,
-    "name": "Ervin Howell",
-    "username": "Antonette",
-    "email": "Shanna@melissa.tv",
-    "address": {
-      "street": "Victor Plains",
-      "suite": "Suite 879",
-      "city": "Wisokyburgh",
-      "zipcode": "90566-7771",
-      "geo": {
-        "lat": "-43.9509",
-        "lng": "-34.4618"
-      }
-    },
-    "phone": "010-692-6593 x09125",
-    "website": "anastasia.net",
-    "company": {
-      "name": "Deckow-Crist",
-      "catchPhrase": "Proactive didactic contingency",
-      "bs": "synergize scalable supply-chains"
-    }
-  }
- 
-
-[Yesterday 3:39 PM] Jonathan Ndambuki
-use destructuring to get the value of lat
+/**
+ * use destructuring to get the value of lat
  */
-
+ 
 let user = {
   id: 2,
   name: "Ervin Howell",
